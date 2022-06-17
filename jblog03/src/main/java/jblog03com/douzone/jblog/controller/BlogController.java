4ca759832c6c957b7ad4f97009032479d6d37646
kwakdo <vv05044@gmail.com>
2022-06-17 16:13:48 +0900

    누가 죄인인가

--- jblog03/src/main/java/jblog03com/douzone/jblog/controller/BlogController.java
+++ jblog03/src/main/java/jblog03com/douzone/jblog/controller/BlogController.java
@@ -1,6 +1,33 @@
 package jblog03com.douzone.jblog.controller;
 
-//@Controller
-public class BlogController {
+import java.util.Optional;
 
+@Controller
+
+@RequestMapping("/{id:(?!assets).*}")
+public class BlogController {
+	
+	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"})
+	public String index(
+			@PathVariable("id") String id,
+			@PathVariable("pathNo1") Optional<Long> pathNo1,
+			@PathVariable("pathNo2") Optional<Long> pathNo2) {
+			
+			Long categoryNo = 0L;
+			Long postNo = 0L;
+		
+			if(pathNo2.isPresent()) {
+				categoryNo = pathNo1.get();
+				postNo = pathNo2.get();
+			} else if(pathNo1.isPresent()) {
+				categoryNo = pathNo1.get();
+			}
+		
+			categoryService.getCategories(id, categoryNo);
+	}
+	
+	@RequestMapping("/admin/basic")
+	public String adminBasic(@PathVariable("id") String id) {
+		
+	}
 }
